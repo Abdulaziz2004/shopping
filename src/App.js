f5c7d83deb1ce87a86a780c83ad4d4968ca51fe5
Abdulaziz2004 <abu556626@gmail.com>
2022-03-31 10:32:43 +0500

    add

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
+import styled from 'styled-components';
+
+
+function App() {
+  return (
+    <AppContainer>
+      <Routes>
+        
+      </Routes>
+      
+    </AppContainer>
+  );
+}
+
+export default App;
+
+const AppContainer = styled.div`
+  padding: 35px 0 35px 16px;
+  display: flex;
+  height: 100vh;
+  /* justify-content: space-between; */
+`
